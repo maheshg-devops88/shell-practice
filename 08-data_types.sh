6197eefe78b4a8f7b8f0f7301a142e58315efcd1
mahesh <mahesh9.storage@gmail.com>
2026-07-16 13:14:53 +0530

    variables script

--- 08-data_types.sh
+++ 08-data_types.sh
@@ -6,3 +6,10 @@ NUM2=Mahesh
 SUM=$((NUM1+NUM2))
 
 echo "SUM is : $SUM"
+
+CARS = ("Thar", "Seltos" , "Gimny")  #This is an array 
+
+echo "Cars are : ${CARS$[@]}
+echo "First car is : ${CARS[0]}"
+echo "First car is : ${CARS[1]}"
+echo "First car is : ${CARS[2]}"
