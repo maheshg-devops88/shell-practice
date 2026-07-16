dc4c86b99af4d0d9320f0044e4205a05a8f6b2e3
mahesh <mahesh9.storage@gmail.com>
2026-07-16 13:25:40 +0530

    variables script

--- 09-condtions.sh
+++ 09-condtions.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-Num=10
+Num=$1
 
 if [ $Num -gt 20 ]; then
 
     echo "$Num is greater than 20"
+
+elif [ $Num -eq 20 ]; then
+    
+    echo "$Num is equal to 20"
 else
    
     echo "$Num is less than 20"
