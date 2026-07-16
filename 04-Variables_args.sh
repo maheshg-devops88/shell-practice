48fd39af83d46d77efec934ffdbc334da84a01ed
mahesh <mahesh9.storage@gmail.com>
2026-07-16 11:34:47 +0530

    new scripts added

---
+++ 04-Variables_args.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# $1 and $2 are arguments 
+
+Person1=$1
+Person2=$2
+
+echo "$Person1: How are you $Person2"
+echo "$Person2: I am good How are you $Person1"
+echo "$Person1: What are you doing $Person2"
+echo "$Person2: I am learning Devops"
