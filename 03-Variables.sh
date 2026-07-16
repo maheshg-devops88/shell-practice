1ce9655379ac72f26242ccff2270c71875567720
mahesh <mahesh9.storage@gmail.com>
2026-07-16 11:31:49 +0530

    new scripts added

---
+++ 03-Variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+Person1=Nagesh
+Person2=Kiran
+
+echo "$Person1: How are you $Person2"
+echo "$Person2: I am good How are you $Person1"
+echo "$Person1: What are you doing $Person2"
+echo "$Person2: I am learning Devops"
