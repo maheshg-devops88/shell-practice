91fcdb3c5cce54f1ad11987439cebe7f9c7c545d
mahesh <mahesh9.storage@gmail.com>
2026-07-16 12:20:31 +0530

    variables script

--- 07-special_variables.sh
+++ 07-special_variables.sh
@@ -5,7 +5,7 @@ echo "Number of arguments passwd to a script: $#"
 echo "Script Name: $0"
 echo "Present which directory you are in: $PWD"
 echo "Who is running the script: $USER"
-echo "Home Directory of the user: $Home"
+echo "Home Directory of the user: $HOME"
 echo "PID of the Script: $$"
 sleep 30 &
 echo "Background process id: $!"
