ea7707ef906f2d47add13a8ccd575f26fee98a9a
mahesh <mahesh9.storage@gmail.com>
2026-07-16 11:40:25 +0530

    variables script

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 echo "Enter the UserName:"
-read USER_Name
+read User_Name
 
 echo "Enter the Password:"
 #read Password                 # it will print output to the Terminal
 read -s Password
+
+echo "username is $User_Name and password is $Password" 
