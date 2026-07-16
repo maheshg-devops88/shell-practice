b899ecf00b7d945dfe3f42dc3d0b43d1199d6ace
mahesh <mahesh9.storage@gmail.com>
2026-07-16 11:57:04 +0530

    variables script

--- 06-variables.sh
+++ 06-variables.sh
@@ -4,5 +4,10 @@
 
 Start_Time=$(date +%S)
 
+sleep 10
 
-echo "$Start_Time"
+End_Time=$(date +%$)
+
+Total_Time=$(($End_Time-$Start_Time))
+
+echo "Total Time taken for script execution::$Total_Time"
