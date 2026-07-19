07f1ce96981589f779308ab896a16cdfb4a119cc
mahesh <mahesh9.storage@gmail.com>
2026-07-19 19:48:45 +0530

    added functions script

---
+++ 11-functions.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+Userid=$(id -u)
+
+if [ $Userid -ne 0 ]; then
+   echo "Run the script with sudo permissions"
+   exit 1
+fi
+
+VALIDATE () {
+    if [ $1 -ne 0 ]; then
+       echo "$2 installation is failure"
+       else
+       echo "$1 installation is success"
+}
+
+
+dnf install nginx -y
+VALIDATE $? "nginx"
+
+
