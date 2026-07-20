ed9f9eeed70e27481e87b718d2692570c934674b
mahesh <mahesh9.storage@gmail.com>
2026-07-20 12:46:05 +0530

    loops scripts

---
+++ 15-loops.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+user_id=$(id -u)
+
+LOGS_FOLDER=/var/logs/shell-script
+LOGS_FILE=/var/logs/shell-script/$0.logs
+
+if [ $user_id -ne 0 ]; then
+   echo "Please execute the script with sudo or root access"
+   exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE () {
+    if [ $1 -ne 0 ]; then
+       echo "$2 installation is failure" | tee -a $LOGS_FILE
+       exit 1
+       else
+       echo "$2 installation is success" | tee -a  $LOGS_FILE 
+    fi
+}
+
+if [ $# -gt 0 ]; then
+   for package in $@ ; do
+      dnf installed package &>> $LOGS_FILE
+      if [ $? -eq 0 ]; them 
+        echo "$package is aleady installed" | tee -a $LOGS_FILE
+    else
+        echo "Installing..... $package"    
+        dnf install $package -y &>> $LOGS_FILE
+        VALIDATE $? "$package"
+      done
+else 
+   echo "Please provide the packages to execute the script"
+fi
