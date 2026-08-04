c1722d0954579e0a60927089dc05f5f08ac2c260
mahesh <mahesh050917@gmail.com>
2026-08-04 06:12:15 +0530

    added shell scripts

---
+++ 22-delete-old-logs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+user_id=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[34m"
+N="\e[0m"
+
+LOGS_FOLDER=/var/logs/shell-script
+LOGS_FILE=/var/logs/shell-script/$0.logs
+
+
+FILES=$(find /home/ec2-user/app-logs -type f -mtime +14)
+
+While IFS= read -r $FILES ; do
+     echo "OLD_Files: $FILE"
+     done < "$FILES"
