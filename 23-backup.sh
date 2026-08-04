7388798f20583fb8fa7a93bf2f5ad9e0b701fc25
mahesh <mahesh9.storage@gmail.com>
2026-08-04 19:20:01 +0530

    updated shell scripts

--- 23-backup.sh
+++ 23-backup.sh
@@ -22,9 +22,13 @@ fi
 mkdir -p $LOGS_FOLDER
 
 USAGE() {
-    echo -e "$Y Please provide ### sh backup.sh SRC_DIR DEST_DIR NO_OF_DAYS ##### $N" 
+    echo -e "$Y Please provide ### sh backup.sh SRC_DIR DEST_DIR NO_OF_DAYS ##### $N"  | tee -a $LOGS_FILE
 }
 
 if [ $# -lt 2 ]; then
    USAGE
 fi
+
+if [ -z $SRC_DIR ]; then
+   echo -e "$Y No files to archive in the $SRC_DIR $N" | tee -a $LOGS_FILE
+fi 
