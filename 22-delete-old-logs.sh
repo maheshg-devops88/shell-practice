b6e3b9bac92226a792652c750177913a1f390cf0
mahesh <mahesh050917@gmail.com>
2026-08-04 06:53:34 +0530

    added shell scripts

--- 22-delete-old-logs.sh
+++ 22-delete-old-logs.sh
@@ -9,26 +9,26 @@ B="\e[34m"
 N="\e[0m"
 
 LOGS_FOLDER=/var/logs/shell-script
-LOGS_FILE="$LOGS_FOLDER/$0.logs"
+LOGS_FILE="$LOGS_FOLDER/$(basename "$0").logs"
 
 if [ $user_id -ne 0 ]; then
-   echo "Please execute the script with sudo or root access"
+   echo -e "$Y Please execute the script with sudo or root access $N" 
    exit 1
 fi
 
 mkdir -p $LOGS_FOLDER
 
-OLD_FILES_To_DELETE=$(find /home/ec2-user/app-logs -type f -mtime +14) 
+OLD_FILES_TO_DELETE=$(find /home/ec2-user/app-logs -type f -mtime +14) 
 
 
 
-if [ -z "$OLD_FILES_To_DELETE" ]; then
+if [ -z "$OLD_FILES_TO_DELETE" ]; then
     
-    echo "No files older than 14 days"
+    echo -e "$R No files older than 14 days $N"
 else 
     while IFS= read -r FILE; do
-         echo "OLD_Files: $FILE"
-    echo "Deleting Old File: $FILE"
+         echo -e "$OLD_FILES: $G $FILE $N"
+    echo -e "$Y Deleting Old File $N: $G $FILE $N"
     rm -f "$FILE"
-    done <<< "$OLD_FILES_To_DELETE"
+    done <<< "$OLD_FILES_TO_DELETE"
 fi
