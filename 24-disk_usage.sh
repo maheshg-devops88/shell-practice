c8b88f7c040896ae807559daa3809d51e5e493eb
mahesh <mahesh050917@gmail.com>
2026-08-05 06:45:10 +0530

    added shell scripts

---
+++ 24-disk_usage.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+user_id=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[34m"
+N="\e[0m"
+
+LOGS_FOLDER=/var/logs/shell-script
+LOGS_FILE=/var/logs/shell-script/$(basename $0).log
+
+if [ $user_id -ne 0 ]; then
+   echo -e "Please execute the script with sudo or root access" 
+   exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+usage=$(df -h | grep -v Filesystem | awk -F" " '{print $1,$5}')
+
+Threshold=30
+
+echo $usage
