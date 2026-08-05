afadeeaebc1920e6a86154e03b9ce5f6e63b5740
mahesh <mahesh050917@gmail.com>
2026-08-05 06:29:17 +0530

    added shell scripts

--- 23-backup.sh
+++ 23-backup.sh
@@ -66,8 +66,9 @@ else
 
     echo -e "$G Files to be Archived:: $N $FILES" | tee -a $LOGS_FILE
    #  find "$SRC_DIR" -type f -name *.log -mtime +"$DAYS" | tar -czvf "$ARCHIVE_FILE" -T -
-   
-    find "$SRC_DIR" -type f -name "*.log" -mtime +"$DAYS" -print0 | tar --null -czvf "$ARCHIVE_FILE" -C "$SRC_DIR" --files-from=-
+
+    find "$SRC_DIR" -type f -name "*.log" -mtime +"$DAYS" -printf "%P\0" | tar --null -czvf "$ARCHIVE_FILE" -C "$SRC_DIR" --files-from=-
+
     if [ $? -eq 0 ]; then
         echo "Archive completed successfully: $ARCHIVE_FILE"
 
