d2ed1f98a372a442f0fe8d964d70f69281f87a35
mahesh <mahesh9.storage@gmail.com>
2026-08-06 13:40:19 +0530

    added shell scripts

--- 24-disk_usage.sh
+++ 24-disk_usage.sh
@@ -25,9 +25,9 @@ Threshold=30
 echo $usage
 
 for fileystem in $usage 
-   
-   if [ $5 -gt $Threshold ];
+   if [ $5 -gt $Threshold ]; then
        {
-          echo "Disk Utilization is greater than $threshold: $1,$5
+          echo "Disk Utilization is greater than $Threshold: $1,$5
           exit 1
        }
+done
